56821be0e703bd5aed5d3071b335e37f81dcc715
intgrldxdy <titinmiduknyp@gmail.com>
2023-03-28 20:13:55 +0700

    com

--- coba/src/components/Header.vue
+++ coba/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="bg-gradient-to-br from-pink-600 to-rose-500">
-  <h2 class="text-red-500">Hello World</h2>
+  <h2 class="text-red-500">Hello World!!</h2>
 </div>
 </template>
 
