6327f326ef81ef30862338fd1406a19ae56ec711
intgrldxdy <titinmiduknyp@gmail.com>
2023-03-28 20:10:53 +0700

    up

---
+++ coba/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "@/components/Header.vue"
+export default {
+  name: 'App',
+  components:{
+    Header
+  }
+  
+ }
+
+</script>
+
+<style src="./assets/style.css">
+</style>
---
+++ coba/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="bg-gradient-to-br from-pink-600 to-rose-500">
+  <h2 class="text-red-500">Hello World</h2>
+</div>
+</template>
+
+<script>
+export default {
+    name:"HeaderWeb"
+};
+</script>
+
+<style>
+
+</style>
